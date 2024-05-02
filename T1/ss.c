0e191d00c2d9f24cf078cb0154c99efb0cd9010c
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-05-02 15:51:27 -0400

    starting ss functions

--- T1/ss.c
+++ T1/ss.c
@@ -1,7 +1,17 @@
 #include "ss.h"
 #include "mtree.h"
 
-Point *Cluster(Point *P, int b) {
+Point *Cluster(Point *C_in, int C_max, 
+                // Parámetros que no están en el pseudo código
+                // Número de puntos en C_in
+                int num_points) {
+
+    Point **C_out;
+    Point **C;
+
+    for (int i = 0; i < num_points; i++) {
+        
+    }
 
 }
 
