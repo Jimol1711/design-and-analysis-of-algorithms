a919608d23bd10fc449f9c882f9b2007890a05cb
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-05-02 14:31:19 -0400

    added headers and definitions for ss

--- T1/ss.c
+++ T1/ss.c
@@ -1,8 +1,16 @@
 #include "ss.h"
 #include "mtree.h"
 
-Point **cluster() {
-    
+Point *Cluster(Point *P, int b) {
+
+}
+
+Entry *OutputHoja(Point *C_in) {
+
+}
+
+Entry *OutputInterno(Entry *C_mra) {
+
 }
 
 Node *sextonSwinbank(Point *P) {
