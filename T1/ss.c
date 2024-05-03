baf2684e4237b9b7a848a9962da4b89851341651
Jimol1711 <juan.molina.l@ug.uchile.cl>
2024-05-03 19:27:40 -0400

    refactor with pointsarray and clustersarray structs

--- T1/ss.c
+++ T1/ss.c
@@ -3,6 +3,18 @@
 
 #include "mtree.c"
 
+// Structure to store size of point arrays
+typedef struct pointsarray {
+    Point *self;
+    int size;
+} PointsArray;
+
+// Structure to store size of cluster arrays
+typedef struct Clustersarray {
+    Point **self;
+    int size;
+} ClustersArray;
+
 // Función que encuentra el medoide primario en un cluster
 Point primary_medoid(Point *cluster, int cluster_size) {
     double min_radius = DBL_MAX;
@@ -50,13 +62,14 @@ double max(double a, double b) {
     return (a > b) ? a : b;
 }
 
-Point *Cluster(Point *C_in, int C_max, 
+Point **Cluster(Point *C_in, int C_max, 
                 // Parámetros que no están en el pseudo código
                 // Número de puntos en C_in
                 int num_points) {
 
     // Partes 1. y 2.
-    Point *C_out = NULL;
+    PointsArray C_out;
+    C_out.self = NULL;
     Point **C = (struct Point*)malloc(num_points * sizeof(Point));
 
     for (int i = 0; i < num_points; i++) {
@@ -77,7 +90,7 @@ Point *Cluster(Point *C_in, int C_max,
 
             }
         }
-        // WIP
+        // WIP, por aquí se aumenta o disminuye C_out.size
     }
 
     // Parte 4. y 5.
@@ -133,14 +146,73 @@ Entry *OutputHoja(Point *C_in,
     leaf->p = g;
     leaf->cr = r;
     leaf->a = a;
+
     return leaf;
 
 }
 
-Entry *OutputInterno(Entry *C_mra) {
+Entry *OutputInterno(Entry *C_mra,
+                    // Parámetros que no están en el pseudo código
+                    // Cardinalidad de C_mra
+                    int C_mra_size) {
 
+    // Parte 1.
+    Point *C_in = malloc(C_mra_size * sizeof(Point));
+    int C_in_size = C_mra_size;
+
+    for (int i = 0; i < C_mra_size; i++) {
+        C_in[i] = C_mra[i].p;
+    }
+
+    Point G = primary_medoid(C_in, C_in_size);
+    int R = 0;
+    Entry *C = malloc(C_mra_size * sizeof(Entry));
+
+    // Parte 2.
+    for (int i = 0; i < C_mra_size; i++) {
+        C[i] = C_mra[i];
+        R = max(R, euclidean_distance(G, C_mra[i].p) + C_mra[i].cr);
+    }
+
+    // Parte 3.
+    Entry *A = &C;
+
+    // Parte 4.
+    Entry *internal;
+    internal->p = G;
+    internal->cr = R;
+    internal->a = A;
+
+    free(C_in);
+    free(C);
+
+    return internal;
 }
 
-Node *sextonSwinbank(Point *P) {
+Node *sextonSwinbank(Point *C_in,
+                    // Parámetros que no están en el pseudo-código
+                    // Cardinalidad de C_in
+                    int C_in_size) {
 
+    // Parte 1.
+    if (C_in_size <= B) {
+        Entry *leaf = OutputHoja(C_in, C_in_size);
+        Node *child = leaf->a;
+        return child;
+    }
+
+    // Parte 2.
+    PointsArray c_out_pts = Cluster(C_in, B, C_in_size);
+    Point *C_out = c_out_pts.self;
+    int C_out_size = c_out_pts.size;
+
+    PointsArray c_pts;
+    Point *C = c_pts.self;
+    int C_size = 0;
+
+    for (int i = 0; i < C_out_size; i++) {
+        Point c = C_out[i];
+        C[i] = OutputHoja();
+    }
+    
 }
