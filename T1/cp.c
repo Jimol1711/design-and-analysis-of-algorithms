c1b04ce5714b9f5d09002860fdde1699dc902e47
andyreus17 <andyreus17@gmail.com>
2024-05-03 16:01:23 -0400

    added step 5 and 6 to cp algorithm

--- T1/cp.c
+++ T1/cp.c
@@ -27,16 +27,15 @@ Point* getSamples(Point* point_set, int set_size, int sample_size) {
     return samples; // return the samples array
 }
 
-Point* deleteElemenFromArray(Point** array, int index, int* array_size) {
-    for (int i=index; i < *array_size - 1; i++) {
+void deleteElemenFromArray(Point** array, int index, int array_size) {
+    for (int i=index; i < array_size - 1; i++) {
         (*array)[i] = (*array)[i+1];
     }
-    // reduce the array size
-    (*array_size)--;
 }
 
 Node cpBulkLoading(Point* point_set, int set_size) {
-    // If the number of points in the point set is less or equal to B. STEP 1
+    // STEP 1
+    // If the number of points in the point set is less or equal to B.
     if (set_size <= B) {
         // Then create a new Node structure (it will be a leaf)
         Node newNode;
@@ -55,69 +54,87 @@ Node cpBulkLoading(Point* point_set, int set_size) {
     }
 
     else {
-        int sample_size = min(B, set_size/B); // size of the set of samples. STEP 2
-        Point* F = getSamples(point_set, set_size, sample_size); // Get the samples array
+        int sample_size; // declaration of sample size variable
 
         SubsetStructure samples_subsets[sample_size]; // array that contains the samples subsets structure for each sample point in F. STEP 3
         // for each point in the sample subset, create the sample structure 
         for (int i=0; i<sample_size; i++) {
             SubsetStructure newSubsetStructure;
-            newSubsetStructure.point = F[i];
             newSubsetStructure.subset_size = 0;
             newSubsetStructure.sample_subset = NULL;
             samples_subsets[i] = newSubsetStructure;
         }
 
-        // For each point in the point set, assign to the nearest sample
-        for (int i=0; i<set_size; i++) {
-            double nearest_distance = euclidian_distance(point_set[i], F[0]);
-            int nearest_sample_index = 0;
-            for (int k=1; k<sample_size; k++) {
-                if (euclidian_distance(point_set[i], F[k]) < nearest_distance) {
-                    nearest_distance = euclidian_distance(point_set[i], F[k]);
-                    nearest_sample_index = k;
+        do {
+            // STEP 2
+            sample_size = min(B, set_size/B); // size of the set of samples.
+            Point* F = getSamples(point_set, set_size, sample_size); // Get the samples array
+
+
+            // For each point in the point set, assign to the nearest sample
+            for (int i=0; i<set_size; i++) {
+                double nearest_distance = euclidian_distance(point_set[i], F[0]);
+                int nearest_sample_index = 0;
+                // evaluate for each sample point in F
+                for (int k=1; k<sample_size; k++) {
+                    if (euclidian_distance(point_set[i], F[k]) < nearest_distance) {
+                        nearest_distance = euclidian_distance(point_set[i], F[k]);
+                        nearest_sample_index = k;
+                    }
                 }
+                // Add the point of the set point to the subset of the nearest sample
+                SubsetStructure* subset_structure = &(samples_subsets[nearest_sample_index]); // Obtain a pointer to the structure that references the subset of the nearest sample found
+                int subset_size = subset_structure->subset_size;
+                subset_structure->sample_subset = (Point*)realloc(subset_structure->sample_subset, (subset_size+1) * sizeof(Point));
+                (subset_structure->sample_subset)[subset_size] = point_set[i];
+                subset_structure->subset_size++;
             }
-            SubsetStructure* subset_structure = &(samples_subsets[nearest_sample_index]); // Obtain a pointer to the structure that references the subset of the nearest sample found
-            int subset_size = subset_structure->subset_size;
-            subset_structure->sample_subset = (Point*)realloc(subset_structure->sample_subset, (subset_size+1) * sizeof(Point));
-            (subset_structure->sample_subset)[subset_size] = point_set[i];
-            subset_structure->subset_size++;
-        }
 
-        // Redistribution. STEP 4
-        for (int j=0; j<sample_size; j++) { // for each F_j
-            if (samples_subsets[j].subset_size < b) { // if |F_j| < b
-                deleteElemenFromArray(&F, j, &sample_size); // delete p_j from F (element j from F)
+            // Redistribution. STEP 4
+            for (int j=0; j<sample_size; j++) { // for each F_j
+                SubsetStructure current_substet = samples_subsets[j];
+                if (current_substet.subset_size < b) { // if |F_j| < b
+                    deleteElemenFromArray(&F, j, sample_size); // delete p_j from F (element j from F)
+                    deleteElemenFromArray(&samples_subsets, j, sample_size); // delete subset structure j from sample_subset
+                    sample_size--; // the sample size is reduced by 1
 
-                // for each p in F_j
-                for (int i=0; i < samples_subsets[j].subset_size; i++) {
-                    Point p = (samples_subsets[j].sample_subset)[i]; // get the point p
-                    double nearest_distance = euclidian_distance(p, F[0]);
-                    int nearest_sample_index = 0;
+                    // for each p in F_j
+                    for (int i=0; i < current_substet.subset_size; i++) {
+                        Point p = (current_substet.sample_subset)[i]; // get the point p
+                        double nearest_distance = euclidian_distance(p, F[0]);
+                        int nearest_sample_index = 0;
 
-                    // Found the nearest subset for the point p
-                    for (int k = 1; k < sample_size; k++) {
-                        double distance = euclidian_distance(p, F[k]);
-                        if (distance < nearest_distance) {
-                            nearest_distance = distance;
-                            nearest_sample_index = k;
+                        // Found the nearest subset for the point p
+                        for (int k = 1; k < sample_size; k++) {
+                            double distance = euclidian_distance(p, F[k]);
+                            if (distance < nearest_distance) {
+                                nearest_distance = distance;
+                                nearest_sample_index = k;
+                            }
                         }
+                        // Move p point to the nearest subset (F_l)
+                        SubsetStructure *subset_structure = &(samples_subsets[nearest_sample_index]);
+                        int subset_size = subset_structure->subset_size;
+                        subset_structure->sample_subset = (Point *)realloc(subset_structure->sample_subset, (subset_size + 1) * sizeof(Point));
+                        (subset_structure->sample_subset)[subset_size] = p;
+                        subset_structure->subset_size++;
                     }
 
-                    // Move p point to the nearest subset (F_l)
-                    SubsetStructure *subset_structure = &(samples_subsets[nearest_sample_index]);
-                    int subset_size = subset_structure->subset_size;
-                    subset_structure->sample_subset = (Point *)realloc(subset_structure->sample_subset, (subset_size + 1) * sizeof(Point));
-                    (subset_structure->sample_subset)[subset_size] = p;
-                    subset_structure->subset_size++;
+                    // Free memory from the sample set being redistributed
+                    free((current_substet.sample_subset));
                 }
-
-                // Free memory from the sample set being redistributed
-                free((samples_subsets[j].sample_subset));
             }
+        } while (sample_size == 1); // if the sample size |F| = 1, return to step 2
+
+        // STEP 6
+        Node trees[sample_size];
+        for (int i = 0; i < sample_size; i++) {
+            // Recursively call cpBulkLoading for each subset F_j
+            trees[i] = cpBulkLoading(samples_subsets[i].sample_subset, samples_subsets[i].subset_size);
         }
 
-        // Hacer un do while...
+        // STEP 7: if the root of the tree is less than b ...
     }
+
+
 }
