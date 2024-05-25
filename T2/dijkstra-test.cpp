1a08ec5f4c4e83d96884f358ee3c7836a22612f1
Vichito22 <vicente.leyton.c@ug.uchile.cl>
2024-05-25 14:38:15 -0400

    FIX: graph represented as an adjacency list

--- T2/dijkstra-test.cpp
+++ T2/dijkstra-test.cpp
@@ -3,49 +3,98 @@
 
 using namespace std;
 
-struct Edge {
-    int src, dest;
-    double weight;
-};
+class Graph {
+private:
+    vector<vector<pair<int, double>>> adjList;
+    int numVertices;
 
-// Función para generar grafos sintéticos
-vector<Edge> generateGraph(int v, int e) {
-    vector<Edge> graph;
-
-    // Generar árbol cobertor
-    for (int i = 1; i < v; ++i) {
-        Edge edge;
-        edge.src = i;
-        edge.dest = rand() % i; // Nodo aleatorio en [0, i-1]
-        edge.weight = (double)(rand()) / RAND_MAX; // Peso aleatorio en (0, 1]
-        graph.push_back(edge);
+public:
+    // Constructor
+    Graph(int numVertices) : numVertices(numVertices) {
+        adjList.resize(numVertices);
     }
 
-    // Añadir aristas restantes
-    for (int i = v - 1; i < e; ++i) {
-        Edge edge;
-        edge.src = rand() % v;
-        edge.dest = rand() % v;
-        edge.weight = (double)(rand()) / RAND_MAX; // Peso aleatorio en (0, 1]
-        graph.push_back(edge);
+    // Añadir una arista con un peso
+    void addEdge(int u, int v, double weight) {
+        adjList[u].emplace_back(v, weight);
+        adjList[v].emplace_back(u, weight);  // No dirigido
+    }
+
+    // Mostrar el grafo
+    void printGraph() const {
+        for (int i = 0; i < numVertices; ++i) {
+            cout << i << ": ";
+            for (const auto& neighbor : adjList[i]) {
+                cout << "(" << neighbor.first << ", " << neighbor.second << ") ";
+            }
+            cout << endl;
+        }
+    }
+};
+
+// Función para generar un peso aleatorio en el rango (0, 1]
+double getRandomWeight() {
+    static mt19937 gen(random_device{}());
+    static uniform_real_distribution<> dis(0.0001, 1.0);
+    return dis(gen);
+}
+
+// Función para generar un grafo aleatorio conexo
+Graph generateRandomGraph(int v, int e) {
+    Graph graph(v);
+    set<pair<int, int>> existingEdges; // Para no repetir aristas
+
+    // Crear un árbol cobertor mínimo para garantizar la conectividad
+    for (int i = 1; i < v; ++i) {
+        int randomNode = rand() % i;
+        double weight = getRandomWeight();
+        graph.addEdge(i, randomNode, weight);
+        existingEdges.emplace(min(i, randomNode), max(i, randomNode));
+    }
+
+    // Crear una lista de todas las posibles aristas adicionales
+    vector<pair<int, int>> possibleEdges;
+    for (int i = 0; i < v; ++i) {
+        for (int j = i + 1; j < v; ++j) {
+            if (existingEdges.find({i, j}) == existingEdges.end()) {
+                possibleEdges.emplace_back(i, j);
+            }
+        }
+    }
+
+    // Mezclar las aristas posibles para extraerlas luego aleatoriamente
+    shuffle(possibleEdges.begin(), possibleEdges.end(), mt19937(random_device()()));
+
+    // Añadir las aristas restantes hasta alcanzar e aristas en total
+    int additionalEdges = e - (v - 1);
+    for (int i = 0; i < additionalEdges && i < possibleEdges.size(); ++i) {
+        int u = possibleEdges[i].first;
+        int v = possibleEdges[i].second;
+        double weight = getRandomWeight();
+        graph.addEdge(u, v, weight);
+        existingEdges.emplace(min(u, v), max(u, v));
     }
 
     return graph;
 }
 
 int main() {
-    srand(time(0)); // Semilla aleatoria
+    // Definir parámetros
+    int i = 12;  // Puedes cambiar a 12 o 14
+    int j = 16;  // Puedes cambiar a un valor entre 16 y 22
 
-    int v = 4; // Número de nodos
-    int e = 7; // Número de aristas
+    // Número de nodos y aristas
+    int v = pow(2, i);
+    int e = pow(2, j);
 
-    vector<Edge> graph = generateGraph(v, e);
+    // Inicializar la semilla para rand()
+    srand(static_cast<unsigned int>(time(nullptr)));
 
-    // Imprimir el grafo generado
-    cout << "Grafo Generado:" << endl;
-    for (const auto& edge : graph) {
-        cout << edge.src << " -> " << edge.dest << " : " << edge.weight << endl;
-    }
+    // Generar el grafo
+    Graph graph = generateRandomGraph(v, e);
+
+    // Imprimir el grafo
+    graph.printGraph();
 
     return 0;
 }
