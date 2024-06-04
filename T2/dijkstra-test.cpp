d09041aa9a6a39f2d94fe5a0e61877e3278cd03b
andyreus17 <andyreus17@gmail.com>
2024-06-03 20:15:10 -0400

    pre-testing

--- T2/dijkstra-test.cpp
+++ T2/dijkstra-test.cpp
@@ -7,6 +7,7 @@ int main() {
     // Inicializar la semilla para rand()
     srand(static_cast<unsigned int>(time(nullptr)));
 
+    #if 0
     // Inicializar arreglos para almacenar el tiempo de ejecucion para cada (i,j)
     std::map<int, std::map<int, vector<double>>> heap_times, fibonacci_times;
 
@@ -17,12 +18,13 @@ int main() {
             fibonacci_times[i][j].reserve(50); // Reserva espacio para 50 elementos en cada subvector
         }
     }
+    #endif
 
     // Para ejecutar el algoritmo en cada par (i,j)
     for (int i = 10; i <= 14; i+=2) {
         for (int j = 16; j <= 22; j++) {
             cout << "--------------- Caso i=" << i << ", j=" << j << ": ---------------"<< std::endl;
-            for (int k = 0; k < 50; k++) { // k < 1 para testear como funciona, debería ser k < 50
+            for (int k = 0; k < 1; k++) { // k < 1 para testear como funciona, debería ser k < 50
                 cout << "----- Iteración " << k+1 << std::endl;
                 int v = pow(2, i);
                 int e = pow(2, j);
@@ -30,21 +32,22 @@ int main() {
                 Graph graph = generateRandomGraph(v, e);
                 // graph.printGraph(); // Por si es necesario ver el grafo
                 auto start1 = chrono::high_resolution_clock::now(); // Registro de tiempo de inicio heap
-                //dijkstraHeap(graph, raiz);
+                dijkstraHeap(graph, raiz);
                 auto end1 = chrono::high_resolution_clock::now();   // Registro de tiempo de finalización heap
-                auto start2 = chrono::high_resolution_clock::now(); // Registro de tiempo de inicio fibonacci
+                // auto start2 = chrono::high_resolution_clock::now(); // Registro de tiempo de inicio fibonacci
                 //dijkstraFibonacci(graph, raiz);
-                auto end2 = chrono::high_resolution_clock::now();   // Registro de tiempo de finalización fibonacci
+                // auto end2 = chrono::high_resolution_clock::now();   // Registro de tiempo de finalización fibonacci
                 chrono::duration<double> duration1 = end1 - start1;   // Cálculo del tiempo transcurrido heap
-                chrono::duration<double> duration2 = end2 - start2;   // Cálculo del tiempo transcurrido fibonacci
+                // chrono::duration<double> duration2 = end2 - start2;   // Cálculo del tiempo transcurrido fibonacci
                 cout << "Tiempo del algoritmo con heap: " << duration1.count() << " segundos" << std::endl;
-                cout << "Tiempo del algoritmo con cola de fibonacci: " << duration2.count() << " segundos" << std::endl;
-                heap_times[i][j].push_back(duration1.count());
-                fibonacci_times[i][j].push_back(duration2.count());
+                // cout << "Tiempo del algoritmo con cola de fibonacci: " << duration2.count() << " segundos" << std::endl;
+                // heap_times[i][j].push_back(duration1.count());
+                // fibonacci_times[i][j].push_back(duration2.count());
             }
         }
     }
 
+    #if 0
     // Imprimir los arreglos del mapa heap_times
     for (const auto& outerPair : heap_times) {
         std::cout << "i = " << outerPair.first << std::endl;
@@ -68,6 +71,7 @@ int main() {
             std::cout << "]" << std::endl;
         }
     }
+    #endif
 
 
     return 0;
