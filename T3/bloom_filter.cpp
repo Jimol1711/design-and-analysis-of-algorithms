777e706b27bb6a126bdafac28343271724a5b508
andyreus17 <andyreus17@gmail.com>
2024-07-19 14:18:45 -0400

    update: hash files and bloom filter

--- T3/bloom_filter.cpp
+++ T3/bloom_filter.cpp
@@ -3,8 +3,9 @@
 #include <vector>
 #include <fstream>
 #include <sstream>
-#include "./hashing/cityhash/src/city.h" // importar cityHash
-#include "./hashing/smhasher/src/MurmurHash2.h" // importar murmurHash
+#include "City.cpp" // importar cityHash
+#include "MurmurHash2.cpp" // importar murmurHash
+
 
 using namespace std;
 
@@ -16,11 +17,52 @@ using namespace std;
  * funciones de hash adicional a partir de las dos funciones de hash base de la siguiente forma:
  * g_i = h_1(x) + i*h_2(x).
  * 
+ * Otra manera sería crear una familia universal (como aparece en el apunte),
+ * aunque habría que ver como encontrar un primo >= N, con N el tamaño del Universo. ¿...?
+ * 
  * Las funciones de hash escogidas son arbitrarias, obviamente, tomando algunas conocidas por 
  * su rapidez y buena dispersión. Además, es preferible que sean no criptográficas, pues tienen
  * menor complejidad y menor tiempor de cálculo y la seguridad no es una prioridad (como en el 
  * caso de codificar contraseñas o algo por el estilo).
  */
+
+
+/** Funcion que imprime las palabras que coinciden dentro de un archivo mediante un proceso
+ * tipo grep (ve cuales palabras coinciden con alguna linea en el archivo e imprime esas palabras)
+ * 
+ * Parametros:
+ * filename: el archivo en el cual se realizará la búsqueda
+ * N : Secuencia de palabras representado por un vector, donde sus elementos son strings.
+ */
+void grep(const string& filename, const vector<string>& N) {
+    // Abrimos el archivo
+    std::ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error al abrir el archivo." << endl;
+        return;
+    }
+
+    // Variable donde se almacenará los string de cada linea
+    string line;
+
+    // Para cada palabra en N, realizamos si coincide con alguna línea en el archivo abierto
+    for (auto& word : N) {
+
+        // Reiniciar el indicador de posición al inicio del archivo
+        file.clear();  // Limpiar cualquier bandera de error del archivo
+        file.seekg(0, ios::beg);  // Mover el indicador de posición al inicio
+
+        // Se recorre cada linea del archivo buscando coincidencias con la palabra actual de la iteracion
+        // .find devuelve la posición de la primera aparición de una subcadena. Si no la encuentra, devuelve string::npos
+        while (getline(file, line)) {
+            if (line == word) {
+                cout << line << endl;
+            }
+        }
+    }
+
+    file.close();
+}
 
 class BloomFilter {
 private:
@@ -33,9 +75,10 @@ private:
 public:
     // Constructor de BloomFilter. Asigna los atributos m y k. Además inicializa el array M
     // de tamaño m, con sus celdas en 0 (false).
-    BloomFilter(int array_size, int hash_count, string file_name) : m(array_size), k(hash_count), M(false, m), file_name(file_name) {
-        
-        // Leemos el archivo .csv
+    BloomFilter(int array_size, int hash_count, string file_name) : m(array_size), k(hash_count), M(array_size, false), file_name(file_name) {
+
+
+        // Abrimos el archivo .csv
         std::ifstream file(file_name);
         if (!file.is_open()) {
             cerr << "Error al abrir el archivo CSV." << endl;
@@ -48,29 +91,81 @@ public:
         // Luego, con el resto de las lineas:
         while (getline(file, line)) {
             // Calcula el hash de la línea usando CityHash ( h1(x) )
-            uint32_t city_hash = CityHash32(line.c_str(), line.size());
+
+            uint64 city_hash = CityHash64(line.c_str(), line.size());
             
             // Calcula el hash de la linea usando MurmurHash ( h2(x) )
-            uint32_t murmur_hash = MurmurHash2(line.c_str(), line.size(), 0);
+            uint64_t murmur_hash = MurmurHash64A(line.c_str(), line.size(), 0);
 
             // Simulamos las k funciones de hashing diferentes y ponemos en 1 las celdas que corresponden
             for (int i=0; i<k; i++) {
                 // g_i(x) = h1(x) + i*h2(x)
-                int k_hash_value = (city_hash + i*murmur_hash) % m;
+                int k_hash_value = (city_hash + i*murmur_hash) % m; // 20
 
-                // M[j] = 1, para cada j retornado por cada función de hash
+                // M[j] = 1, para cada j retornado por cada función dse hash
                 M[k_hash_value] = true;
             }
+            
         }
 
         file.close();
 
     }
 
-    // luego crear metodo de búsqueda, etc..
+    /** Método que realiza la búsqueda utilizando filtro de Bloom.
+     * Por cada palabra en la secuencia N, vemos si los indices que devuelvenentregados por las k funciones de 
+     * hash están en 1 (true) en el arreglo M. Si es así, puede que esa palabra realmente esté en el archivo
+     * abierto. En caso contrario, sin error la palabra no está en el archivo, por lo tanto quitamos esa palabra
+     * de la secuencia N (quitamos el elemento del arreglo). Finalmente en N nos quedamos solo con las palabras 
+     * que si pueden estar en el archivo y se aplica grep a N.
+     */
+    void search(vector<string>& N) {
+
+        auto it = N.begin(); // iniciamos iterador en el primer elemento de N
+        // N.end() entrega un iterador al elemento que sigue al último del vector.
+        while (it != N.end()) {
+            // uint32_t city_hash = CityHash32(line.c_str(), line.size());
+            
+            // Calcula el hash de la linea usando MurmurHash ( h2(x) )
+            uint64 city_hash = CityHash64(it->c_str(), it->size());
+            uint64_t murmur_hash = MurmurHash64A(it->c_str(), it->size(), 0);
+
+            // variable que indicará si hay que eliminar el elemento del vector o no
+            bool eliminar = false;
+
+            // Calculamos el valor del hash de la palabra actual del iterador para cada funcion de hash h_i 
+            for (int i = 0; i < k; i++) {
+                // g_i(x) = h1(x) + i*h2(x)
+                int k_hash_value = (city_hash + i * murmur_hash) % m; // 20
+
+                // Si una funcion da 0 (false), entonces la palabra no está y se debe eliminar de N
+                if (!M[k_hash_value]) {
+                    eliminar = true;
+                    break;
+                }
+            }
+
+            if (eliminar)
+                it = N.erase(it);  // Borra el elemento y actualiza el iterador
+            else
+                ++it;  // Avanza al siguiente elemento
+        }
+        
+        // En este punto N solo contiene las palabras que si podrían estar en el archivo, y aplicamos grep a N.
+        grep(this->file_name, N);
+    }
 
 };
 
 int main() {
+
+    vector<string> sequence = {"ANDRES", "JUAN", "VICENTE"};
+
+    grep("csv/Popular-Baby-Names-Final.csv", sequence);
+
+    // BloomFilter filter(100, 5, "csv/Popular-Baby-Names-Final.csv");
+    // filter.search(sequence);
+
     return 0;
+    
 }
